62eec9f4183ce87a7c14b7083575205b364eec7d
Matthew Fogarty <mattempyre@gmail.com>
2024-02-11 16:22:03 +0100

    Adding Image Uploading

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ import { ActiveElement } from '@/types/type';
 import { useMutation, useRedo, useStorage, useUndo } from '@/liveblocks.config';
 import { defaultNavElement } from '@/constants';
 import { handleDelete, handleKeyDown } from '@/lib/key-events';
+import { handleImageUpload } from '@/lib/shapes';
 
 export default function Page() {
   const undo = useUndo();
@@ -30,6 +31,8 @@ export default function Page() {
   const selectedShapeRef = useRef<string | null>(null);
 
   const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
 
   const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -80,6 +83,13 @@ export default function Page() {
       case 'delete':
         handleDelete(fabricRef.current as any, deleteShapeFromStorage);
         setActiveElement(defaultNavElement);
+        break;
+      case 'image':
+        imageInputRef.current?.click();
+        isDrawing.current = false;
+        if (fabricRef.current) {
+          fabricRef.current.isDrawingMode = false;
+        }
         break;
       default:
         break;
@@ -160,6 +170,17 @@ export default function Page() {
       <Navbar
         activeElement={activeElement}
         handleActiveElement={handleActiveElement}
+        imageInputRef={imageInputRef}
+        handleImageUpload={(e: any) => {
+          e.stopPropagation();
+
+          handleImageUpload({
+            file: e.target.files[0],
+            canvas: fabricRef as any,
+            shapeRef,
+            syncShapeInStorage,
+          });
+        }}
       />
       <section className="flex h-full flex-row">
         <LeftSidebar allShapes={Array.from(canvasObjects)} />
