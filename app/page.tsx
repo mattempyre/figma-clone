a6a56c7f904f759711ea31baa71385fb1f720e88
Matthew Fogarty <mattempyre@gmail.com>
2024-02-11 14:22:17 +0100

    adding ability to add rectangle to canvas

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,44 @@ import LeftSidebar from '@/components/LeftSidebar';
 import Live from '@/components/Live';
 import Navbar from '@/components/Navbar';
 import RightSidebar from '@/components/RightSidebar';
-import { CustomFabricObject } from '@/types/type';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from '@/lib/canvas';
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>('rectangle');
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on('mouse:down', (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      });
+    });
+    window.addEventListener('resize', () => {
+      handleResize({ fabricRef });
+    });
+  }, []);
 
   return (
     <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex h-full flex-row">
         <LeftSidebar />
-        <Live />
+        <Live canvasRef={canvasRef} />
         <RightSidebar />
       </section>
     </main>
--- components/Live.tsx
+++ components/Live.tsx
@@ -12,7 +12,11 @@ import ReactionSelector from './reaction/ReactionButton';
 import FlyingReaction from './reaction/FlyingReaction';
 import useInterval from '@/hooks/useInterval';
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
@@ -156,13 +160,14 @@ const Live = () => {
 
   return (
     <div
+      id="canvas"
       className="h-[100vh] w-full flex justify-center items-center text-center"
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
     >
-      <canvas />
+      <canvas ref={canvasRef} />
 
       {reactions.map((r) => (
         <FlyingReaction
